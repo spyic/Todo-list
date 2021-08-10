dcfe3b1a7ec458aa6bbc5ffc97f4870f84d21aeb
Ashwani Sahani <sahaniashwani00@gmail.com>
2021-08-10 11:22:22 +0530

    This is todo list

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const bodyparser = require("body-parser")
+const app = express()
+var items = [];
+app.set('view engine','ejs');
+app.use(bodyparser.urlencoded({extended: true}))
+app.use(express.static("public"));
+app.get("/",function(req, res){
+var today = new Date();
+
+
+
+var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+};
+
+ var day = today.toLocaleDateString("en-US", options);
+res.render("list", { 
+    kind: day,  newslist: items
+ });
+})
+app.post("/", function(req,res){
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+})
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+})
